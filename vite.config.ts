6ade4c03c322532f42e0eb7303bce5761ddf3188
ValentioD <145469375+ValentioD@users.noreply.github.com>
2025-06-22 23:33:16 +0200

    Fix: correcte base ingesteld voor GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/prg1-04-Eindopdracht/', // <- let op de slash aan het begin en eind!
+  base: '/prg1-04-Eindopdracht/', // let op de leading + trailing slash
 })
